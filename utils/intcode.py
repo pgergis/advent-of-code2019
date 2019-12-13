d93356adbc09c180a327b93c9b72d60a74243b28
Pierre Gergis <pierre.s.gergis@gmail.com>
2019-12-12 22:56:55 -0500

    cleanup / update intcode for selective halt

--- utils/intcode.py
+++ utils/intcode.py
@@ -47,21 +47,71 @@ class IntcodeComputer:
         self.registers = IntcodeComputer.initialize_tape(self.input_str)
         self.ptr = 0
 
-    def run(self, reset=True):
+    def run(self, halt_on_output=False):
         while (instruction := Instruction(str(self.getVal(self.ptr)))) :
             if instruction.opcode == Instruction.TERMINATE_CODE:
+                self.reset()
                 break
-            self.ptr = self.performInstruction(instruction.fn, instruction.param_modes)
-            if self.ptr >= len(self.registers):
-                raise TypeError("Out of registers!!")
 
-        final_state = self.registers
-        if reset:
-            self.reset()
+            self.ptr = self.performInstruction(instruction.fn, instruction.param_modes)
+
+            if self.ptr >= len(self.registers):
+                print("Out of registers!!")
+                return -1
+
+            if halt_on_output and instruction.opcode == Instruction.OUTPUT_CODE:
+                return 1
+        return 0
 
     def set_inputs(self, noun, verb):
         self.setVal(1, noun)
         self.setVal(2, verb)
+
+    @staticmethod
+    def initialize_tape(input_str):
+        return list(map(int, input_str.split(",")))
+
+
+class Instruction:
+    TERMINATE_CODE = 99
+    OUTPUT_CODE = 4
+
+    def __init__(self, instruction):
+        self.OPCODE_FN_MAP = {
+            1: (Instruction.opcode_add, "100"),
+            2: (Instruction.opcode_mult, "100"),
+            3: (Instruction.opcode_input, "1",),
+            self.OUTPUT_CODE: (Instruction.opcode_output, "0"),
+            5: (Instruction.opcode_jump_if_true, "00"),
+            6: (Instruction.opcode_jump_if_false, "00"),
+            7: (Instruction.opcode_less_than, "100"),
+            8: (Instruction.opcode_eq, "100"),
+            9: (Instruction.opcode_adjust_relative_base, "0"),
+            self.TERMINATE_CODE: (lambda args: None, "0"),
+        }
+
+        op_in, params_in = (
+            instruction[-OPCODE_LENGTH:],
+            instruction[:-OPCODE_LENGTH].zfill(1),
+        )
+        self.opcode = int(op_in)
+
+        self.fn, default_param_modes = self.getFnForOpcode()
+        set_params = [
+            int(p_in) + int(def_in)
+            for p_in, def_in in zip(
+                params_in.zfill(len(default_param_modes)), default_param_modes
+            )
+        ]
+        self.param_modes = [ParameterMode(p) for p in set_params]
+
+    def getFnForOpcode(self):
+        # returns mapped function and default parameter modes in a tuple
+        try:
+            op_fn, default_params = self.OPCODE_FN_MAP[self.opcode]
+            return op_fn, default_params
+        except Exception:
+            raise ValueError(f"Invalid Opcode!", self.opcode)
 
     @staticmethod
     def opcode_add(x, y, dest, tape):
@@ -75,12 +125,12 @@ class IntcodeComputer:
 
     @staticmethod
     def opcode_output(val, tape):
-        IntcodeComputer.write_external_output(val, tape)
+        Instruction.write_external_output(val, tape)
         return tape.ptr + 1
 
     @staticmethod
     def opcode_input(dest, tape):
-        val_in = int(IntcodeComputer.get_external_input(tape))
+        val_in = int(Instruction.get_external_input(tape))
         tape.setVal(dest, val_in)
         return tape.ptr + 1
 
@@ -112,63 +162,14 @@ class IntcodeComputer:
         return tape.ptr + 1
 
     @staticmethod
-    def initialize_tape(input_str):
-        return list(map(int, input_str.split(",")))
-
-    @staticmethod
     def get_external_input(tape):
-        if (io_buffer := getattr(tape, "io_buffer")):
+        if (io_buffer := getattr(tape, "io_buffer")) :
             return io_buffer.pop()
         return input("taking input: ")
 
     @staticmethod
     def write_external_output(output, tape):
         if (io_buffer := getattr(tape, "io_buffer")) is not None:
-            orig_next = None
-            if io_buffer:
-                orig_next = io_buffer.pop()
             io_buffer.append(output)
-            if orig_next is not None:
-                io_buffer.append(orig_next)
         else:
             print(output)
-
-
-class Instruction:
-    TERMINATE_CODE = 99
-    OPCODE_FN_MAP = {
-        1: (IntcodeComputer.opcode_add, "100"),
-        2: (IntcodeComputer.opcode_mult, "100"),
-        3: (IntcodeComputer.opcode_input, "1",),
-        4: (IntcodeComputer.opcode_output, "0"),
-        5: (IntcodeComputer.opcode_jump_if_true, "00"),
-        6: (IntcodeComputer.opcode_jump_if_false, "00"),
-        7: (IntcodeComputer.opcode_less_than, "100"),
-        8: (IntcodeComputer.opcode_eq, "100"),
-        9: (IntcodeComputer.opcode_adjust_relative_base, "0"),
-        TERMINATE_CODE: (lambda args: None, "0"),
-    }
-
-    def __init__(self, instruction):
-        op_in, params_in = (
-            instruction[-OPCODE_LENGTH:],
-            instruction[:-OPCODE_LENGTH].zfill(1),
-        )
-        self.opcode = int(op_in)
-
-        self.fn, default_param_modes = self.getFnForOpcode()
-        set_params = [
-            int(p_in) + int(def_in)
-            for p_in, def_in in zip(
-                params_in.zfill(len(default_param_modes)), default_param_modes
-            )
-        ]
-        self.param_modes = [ParameterMode(p) for p in set_params]
-
-    def getFnForOpcode(self):
-        # returns mapped function and default parameter modes in a tuple
-        try:
-            op_fn, default_params = self.OPCODE_FN_MAP[self.opcode]
-            return op_fn, default_params
-        except Exception:
-            raise ValueError(f"Invalid Opcode!", self.opcode)
