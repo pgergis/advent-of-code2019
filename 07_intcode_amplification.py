2220fe89942ec9e4ab92e810c8e8ede247c7a04d
Pierre Gergis <pierre.s.gergis@gmail.com>
2019-12-12 22:57:15 -0500

    day 7: amplification circuit

--- 07_intcode_amplification.py
+++ 07_intcode_amplification.py
@@ -1,21 +1,32 @@
 import itertools
-from utils.cli import lines_in
 from utils.intcode import IntcodeComputer
 
-a, b, c, d, e = (
-    IntcodeComputer(lines_in),
-    IntcodeComputer(lines_in),
-    IntcodeComputer(lines_in),
-    IntcodeComputer(lines_in),
-    IntcodeComputer(lines_in),
-)
 
-outputs = []
-for io in map(list, itertools.permutations(range(5), 5)):
-    io.insert(-1, 0)
+def run_amp_loop(phase_range, feedback_mode):
+    outputs = []
+    for io_order in map(list, itertools.permutations(phase_range, 5)):
+        io = [0]
+        a, b, c, d, e = (
+            IntcodeComputer(lines_in, io_buffer=io),
+            IntcodeComputer(lines_in, io_buffer=io),
+            IntcodeComputer(lines_in, io_buffer=io),
+            IntcodeComputer(lines_in, io_buffer=io),
+            IntcodeComputer(lines_in, io_buffer=io),
+        )
 
-    a.io_buffer, b.io_buffer, c.io_buffer, d.io_buffer, e.io_buffer = io, io, io, io, io
-    a.run(); b.run(); c.run(); d.run(); e.run()
-    outputs.append(io.pop())
+        not_halted = [a, b, c, d, e]
+        for comp in not_halted:
+            if io_order:
+                io.append(io_order.pop())
+            if comp.run(halt_on_output=feedback_mode) == 1:
+                not_halted.append(comp)
 
-print(max(outputs))
+        outputs.append(io.pop())
+    return max(outputs)
+
+
+if __name__ == "__main__":
+    from utils.cli import lines_in
+
+    print("pt. 1", run_amp_loop(range(5), False))
+    print("pt. 2", run_amp_loop(range(5, 10), True))
