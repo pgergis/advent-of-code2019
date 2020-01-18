264cd270a48da9a22a19ded40a3faa459537c9a2
Pierre Gergis <pierre.s.gergis@gmail.com>
2020-01-18 18:14:21 -0500

    working part 1

--- 06_orbits.py
+++ 06_orbits.py
@@ -1,33 +1,24 @@
-CENTER_OF_MASS = "COM"
+from typing import Dict, List
+
+from utils.cli import lines_in
+
+CENTER_OF_MASS = 'COM'
+
+orbit_list = [tuple(pair.split(')')) for pair in lines_in]
+
+orbit_graph: Dict[str, List[str]] = {}
+for center, orbiter in orbit_list:
+    if center not in orbit_graph:
+        orbit_graph[center] = [orbiter]
+    else:
+        orbit_graph[center].append(orbiter)
 
 
-def build_orbit_graph(orbit_lines):
-    def orbit_to_edge(orbit):
-        return tuple(orbit.split(")"))
-
-    graph = {}
-    for orbit in orbit_lines:
-        center, child = orbit_to_edge(orbit)
-        if center not in graph:
-            graph[center] = []
-        graph[center].append(child)
-
-    return graph
+def orbit_count(node, depth=1):
+    chksum = 0
+    for o in orbit_graph.get(node, []):
+        chksum += depth + orbit_count(o, depth=depth + 1)
+    return chksum
 
 
-def get_all_orbits(center, orbit_graph):
-    return [center] + [
-        get_all_orbits(c, orbit_graph) for c in orbit_graph.get(center, [])
-    ]
-
-
-def count_all_orbits(orbit_graph):
-    full_orbital_walk = get_all_orbits(CENTER_OF_MASS, orbit_graph)
-    return full_orbital_walk
-
-
-
-if __name__ == "__main__":
-    from utils.cli import lines_in
-
-    orbit_graph = build_orbit_graph(lines_in)
+print(orbit_count(CENTER_OF_MASS))
